757fcfd97b4a8997c73968abdb485aca2fdb2ac8
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-11 22:58:42 +0000

    added a README.md file and updated gitignore to ignore .env file

---
+++ download_avatars.js
@@ -0,0 +1,30 @@
+// > GET /repos/jquery/jquery/contributors HTTP/1.1
+// > Host: api.github.com
+// > User-Agent: curl/7.47.0
+// > Accept: */*
+
+// [{
+
+// },
+// {
+  "login": "pbakaus",
+//   "id": 43004,
+  "avatar_url": "https://avatars1.githubusercontent.com/u/43004?v=4",
+//   "gravatar_id": "",
+//   "url": "https://api.github.com/users/pbakaus",
+//   "html_url": "https://github.com/pbakaus",
+//   "followers_url": "https://api.github.com/users/pbakaus/followers",
+//   "following_url": "https://api.github.com/users/pbakaus/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/pbakaus/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/pbakaus/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/pbakaus/subscriptions",
+//   "organizations_url": "https://api.github.com/users/pbakaus/orgs",
+//   "repos_url": "https://api.github.com/users/pbakaus/repos",
+//   "events_url": "https://api.github.com/users/pbakaus/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/pbakaus/received_events",
+//   "type": "User",
+//   "site_admin": false,
+//   "contributions": 9
+// }
+// ]
+
