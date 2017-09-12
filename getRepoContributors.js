09da205a295e106602bbbb28a198c07fdb792d9a
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-12 00:22:00 +0000

    created a function to generate an object for the request function (including personal key)

--- getRepoContributors.js
+++ getRepoContributors.js
@@ -1,38 +1,45 @@
+require("dotenv").config();
 const request = require("request");
-require('dotenv').config();
 
-// const options = {
-//   url: 'https://api.github.com/repos/jquery/jquery/contributors',
-//   qs: {
-//     access_token: process.env.GITHUB_ACCESS_TOKEN,
-//     sort: 'pushed'
-//   }
-//   headers: {
-//     'User-Agent': 'Natasha Urbancic'
-//   }
-// }
-
-function getRepoContributors(repoOwner, repoName, cb) {
-
-  // request(options, function(error, response, body) {
-    
-  //     // guard statement
-  //     if (error) {
-  //       console.log(error);
-  //       return;
-  //     }
-    
-  //     // "avatar_url"
-  //     // "login"
-
-  //     });
-  
-
+function getOptionsForRepo(owner, repo) {
+  const options = {
+    url: `https://api.github.com/repos/${owner}/${repo}/contributors`,
+    qs: {
+      access_token: process.env.GITHUB_TOKEN,
+      sort: "pushed"
+    },
+    headers: {
+      "User-Agent": "GitHub Avatar Downloader -- Student Project"
+    }
+  };
+  return options;
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+function getRepoContributors(repoOwner, repoName, cb) {
+  const options = getOptionsForRepo(repoOwner, repoName);
+
+  request(options, function(error, response, body) {
+    // guard statement
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    // data.forEach(contributor => {
+    //   console.log(contributor.login, contributor.avatar_url);
+    // });
+
+    return data;
+  });
+}
+
+const repoData = getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 
-
+// repoData.forEach(contributor => {
+//   console.log(contributor.login, contributor.avatar_url);
+// });
